1ca9ce6e80d805bc5bde00a4824b62eb1307864f
Alain Chautard <alain@interstate21.com>
2016-04-29 16:02:58 -0700

    Step 1 - "Add a location feature" + bootstrap styling

--- app/app.component.ts
+++ app/app.component.ts
@@ -1,7 +1,30 @@
 import {Component} from 'angular2/core';
+
 @Component({
     selector: 'my-app',
-    template: '<h1>My First Angular 2 App</h1>'
+    template: `
+        <div class="jumbotron">
+          <div class="form-group">
+            <label for="zipcode">Enter your zip code</label>
+            <input type="text" class="form-control" #zipcode placeholder="Zipcode">
+          </div>
+            <button class="btn-sm btn-success" type="submit" (click)="addLocation(zipcode.value)">
+              Add location <span class="glyphicon glyphicon-plus"></span>
+            </button>
+        </div>
+        <div class="row marketing" *ngFor="#location of locations">
+          <h4>{{location}}</h4>
+          <p>My weather data</p>
+        </div>
+
+    `
 })
 export class AppComponent {
+
+    locations : Array = [];
+
+    addLocation(location){
+        this.locations.push(location);
+    }
+
 }
