80b2e516886660c02bb57eeb64afae6b88718ed7
alcfeoh <alain@interstate21.com>
2023-12-08 11:26:59 -0800

    Upgrade to v17

--- src/app/weather.service.ts
+++ src/app/weather.service.ts
@@ -19,15 +19,16 @@ export class WeatherService {
   addCurrentConditions(zipcode: string): void {
     // Here we make a request to get the current conditions data from the API. Note the use of backticks and an expression to insert the zipcode
     this.http.get<CurrentConditions>(`${WeatherService.URL}/weather?zip=${zipcode},us&units=imperial&APPID=${WeatherService.APPID}`)
-      .subscribe(data => this.currentConditions.mutate(conditions => conditions.push({zip: zipcode, data})));
+      .subscribe(data => this.currentConditions.update(conditions => [...conditions, {zip: zipcode, data}]));
   }
 
   removeCurrentConditions(zipcode: string) {
-    this.currentConditions.mutate(conditions => {
+    this.currentConditions.update(conditions => {
       for (let i in conditions) {
         if (conditions[i].zip == zipcode)
           conditions.splice(+i, 1);
       }
+      return conditions;
     })
   }
 
